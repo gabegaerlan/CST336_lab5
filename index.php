f8a276e7cf2684199c15bb4b7bd0b45d76f635c9
Gabe Gaerlan <ggaerlan@csumb.edu>
2017-10-12 21:40:21 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            YOYO NOTHING!
+        </title>
+    </head>
+    <header>hello</header>
+    <body>
+        <?php
+        echo "no";
+        
+        ?>
+    </body>
+</html>
